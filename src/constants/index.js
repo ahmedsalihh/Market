ae6abfdd3308dfa0a8e035f1873ec5cf351dab4d
ahmedsalihh <ahmedsalihh@gmail.com>
2022-02-07 21:41:44 +0300

    Fix typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@ export const sortingItems = [
   { id: 1, label: 'Price low to high' },
   { id: 2, label: 'Price high to low' },
   { id: 3, label: 'New to old' },
-  { id: 4, label: 'old to new' },
+  { id: 4, label: 'Old to new' },
 ];
