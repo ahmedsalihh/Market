1663b48b93da2d55b8fa8973040e81c49b132246
ahmedsalihh <ahmedsalihh@gmail.com>
2022-02-07 17:33:38 +0300

    fix container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  margin-left: 6%;
-  margin-right: 6%;
+  margin-left: 357.5px;
+  margin-right: 357.5px;
 `;
 
 export default Container;
